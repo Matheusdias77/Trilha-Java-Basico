f4990658b8337b84cc35a095e372bfbf954e2b11
Matheusdias77 <heyp692@gmail.com>
2024-09-02 16:57:52 -0300

    Finalizando Desafio

---
+++ DesafioControleFluxo/src/main/java/org/example/Domain/ParametrosInvalidosException.java
@@ -0,0 +1,7 @@
+package org.example.Domain;
+
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(String message) {
+        super(message);
+    }
+}
